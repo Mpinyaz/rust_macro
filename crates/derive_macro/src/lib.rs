026c9521b92ba80117537e9ab9be0c741796c931
Mpinyaz <ebmpinyuri@gmail.com>
2026-05-14 07:17:35 +0200

    init proj

---
+++ crates/derive_macro/src/lib.rs
@@ -0,0 +1,29 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput};
+
+#[proc_macro_derive(Json)]
+pub fn json_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let fields = match input.data {
+        Data::Struct(s) => s
+            .fields
+            .into_iter()
+            .map(|field| field.ident.unwrap().to_string())
+            .collect::<Vec<_>>(),
+        Data::Enum(e) => e
+            .variants
+            .into_iter()
+            .map(|variant| variant.ident.to_string())
+            .collect(),
+        Data::Union(u) => u
+            .fields
+            .named
+            .into_iter()
+            .map(|field| field.ident.unwrap().to_string())
+            .collect(),
+    };
+
+    panic!("\n {:#?}", fields);
+    quote!().into()
+}
---
+++ procmacro/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+    for i in 0..23 {
+        println!("{}", i);
+    }
+}
